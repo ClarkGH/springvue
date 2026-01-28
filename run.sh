1405abc1a4c156dd30f14280d840e9afe84874d7
Clark Hinchcliff <demolisho@hotmail.com>
2026-01-27 21:47:48 -0500

    fleshen out application scripts

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$ROOT"
 
 (cd backend && ./mvnw spring-boot:run "$@")
+
+echo "Done doctor-cool. Open http://localhost:8080 in your browser after the build completes."
